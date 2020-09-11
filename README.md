68d9953ccae649b2ea543598d50a86e1604de5fa
Terd47 <terdlar@gmail.com>
2020-09-11 11:43:41 -0400

    updated code with special character array working and included in passwords and also updated readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # password-generator
 This application will generate random passwords based on a user selected criteria. It runs in the browser and is responsive, and dynamic. It will include an array of letters from a to z and A to Z, special characters such as @,%,+,\\,/,',#,$,^,?,:,`,),(,},{,],[,~,-,-. and also numbers.
 This application will be used to generate strong passwords to to offer a higher level of password security 
+
+This project can be accessed through the github repo link below
+https://github.com/Terd47/password-generator
+live site at 
+https://terd47.github.io/password-generator/
--- assets/js/script.js
+++ assets/js/script.js
@@ -15,53 +15,53 @@ generateBtn.addEventListener("click", writePassword);
 
 //prompt user for password length and properties
 
-function PasswordProperties () {
+function PasswordProperties() {
   // numbers to include in user password
-    var num = "0123456789";
-    // lowerCase charcaters to add to user password
-    var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-    // upperCase characters to add to user password 
-    var upperChar = lowerChar.toLocaleUpperCase();
-    //special characters to add to users password
-    var specialChar = "@%+\\/'!#$^?:,)(}{][~-_.";
-    //variable to store user selection
-    var desiredPassword = "";
+  var num = "0123456789";
+  // lowerCase charcaters to add to user password
+  var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+  // upperCase characters to add to user password 
+  var upperChar = lowerChar.toLocaleUpperCase();
+  //special characters to add to users password
+  var specialChar = "@%+\\/'!#$^?:,)(}{][~-_.";
+  //variable to store user selection
+  var desiredPassword = "";
 
   //askk user for their password character preference
 
-    if (confirm("Will you like numbers in your password?")){
-      desiredPassword += num;
-    }
-    if (confirm("Will you like lower Case letters in your password?")){
-       desiredPassword += lowerChar;
-   
-    }
-    if (confirm("Will you like upper Case characters in your password?")){
-         desiredPassword += upperChar;
-    }
-    if (confirm("Will you like special characters in your password?")){
-            desiredPassword += specialChar;
-    }
-      return desiredPassword;
+  if (confirm("Will you like numbers in your password?")) {
+    desiredPassword += num;
+  }
+  if (confirm("Will you like lower Case letters in your password?")) {
+    desiredPassword += lowerChar;
+
+  }
+  if (confirm("Will you like upper Case characters in your password?")) {
+    desiredPassword += upperChar;
+  }
+  if (confirm("Will you like special characters in your password?")) {
+    desiredPassword += specialChar;
+  }
+  return desiredPassword;
 }
 
 // generate user's secure password with all specified properties
 
-function generatePassword () {
+function generatePassword() {
   var passwordSize = 0;
   var genPassword = PasswordProperties();
 
   // get length of password from user
-  while(passwordSize < 8 || passwordSize > 128){
+  while (passwordSize < 8 || passwordSize > 128) {
     passwordSize = prompt("What is your desired password length?");
-   }
-   //generate random number to match user password length and loop through the strings
-   var randomChar;
-   var securePassword = "";
-    for (i = 0; i < passwordSize ; i++) {
-        // generate random number between 0 and the length of user password preference
-        randomChar = Math.floor(Math.random() * (genPassword.length));
-        securePassword += genPassword[randomChar];
-    }
-   return securePassword;
+  }
+  //generate random number to match user password length and loop through the strings
+  var randomChar;
+  var securePassword = "";
+  for (i = 0; i < passwordSize; i++) {
+    // generate random number between 0 and the length of user password preference
+    randomChar = Math.floor(Math.random() * (genPassword.length));
+    securePassword += genPassword[randomChar];
+  }
+  return securePassword;
 }
